bbdeeffc21b03becb9ceabff4519e1a21ce7586f
kashishsinghyadav <kashishhsinghhh@gmail.com>
2025-02-02 11:13:17 +0530

    Connect db

---
+++ read/write through/app/database.py
@@ -0,0 +1,15 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_db_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="mysql",  # Container name from docker-compose
+            user="root",
+            password="root",
+            database="test_db"
+        )
+        return connection
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
