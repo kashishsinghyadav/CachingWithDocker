ac9909b0b5a31c1505a18b96b6b5bcaa4ff48661
kashishsinghyadav <kashishhsinghhh@gmail.com>
2025-02-02 21:49:01 +0530

    update db.py

--- read/write/app/database.py
+++ read/write/app/database.py
@@ -4,7 +4,7 @@ from mysql.connector import Error
 def create_db_connection():
     try:
         connection = mysql.connector.connect(
-            host="mysql",  # Container name from docker-compose
+            host="mysql_container",  # Container name from docker-compose
             user="root",
             password="root",
             database="test_db"
